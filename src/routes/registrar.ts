582dbe7d10d332dbb5cbca5c54d1433dd2f62eb6
cutsweettea <swagastan@proton.me>
2026-05-22 21:04:50 -0400

    changed options for route registration

--- src/routes/registrar.ts
+++ src/routes/registrar.ts
@@ -1,21 +1,20 @@
 import express from 'express';
 import { generateResponse } from '../util.ts';
 
-interface RouteCallback {
+export interface RouteCallback {
     (req: express.Request, res: express.Response): express.Response
 }
 
 interface RegistrationProps {
     path: string,
     type: RequestType,
-    callback: RouteCallback,
-    requiredBodyValues?: string[]
+    callbackOpts: RouteCallbackOptions,
+    adminCallbackOpts?: RouteCallbackOptions
 }
 
-interface RouteRegisterOptions {
-    adminCallback?: RouteCallback,
-    requiredBodyValuesDefault?: string[],
-    requiredBodyValuesAdmin?: string[]
+interface RouteCallbackOptions {
+    callback: RouteCallback,
+    requiredBodyValues?: string[]
 }
 
 // what the fuck is this
@@ -40,7 +39,7 @@ class RouteRegistrar {
             // checks for required body values if specified
             if(!req.body) return res.status(400).send(generateResponse(false, 'no body brah'));
             for(let i = 0; i < requiredBodyValues.length; i++) {
-                // checks each value to see if in body
+                // checks each value to see if in body, returning 400 status if not
                 let v = requiredBodyValues[i];
                 if(!v) return res.status(400).send(generateResponse(false, 'erm'));
                 if(!(v in req.body)) return res.status(400).send(generateResponse(false, 'missing body value'));
@@ -51,21 +50,21 @@ class RouteRegistrar {
         callback(req, res);
     }
 
-    private registerRoute({ path, type, callback, requiredBodyValues }: RegistrationProps): boolean {
+    private registerRoute({ path, type, callbackOpts }: RegistrationProps): boolean {
         console.log(`registered route "${path}" w type ${type.toString()}`);
         // auughh switch case case case
         switch(type) {
             case RequestType.GET: 
-                this.serv.router.get(path, (req, res) => this.defaultHandler(req, res, callback, requiredBodyValues));
+                this.serv.router.get(path, (req, res) => this.defaultHandler(req, res, callbackOpts.callback, callbackOpts.requiredBodyValues));
                 break;
             case RequestType.POST: 
-                this.serv.router.post(path, (req, res) => this.defaultHandler(req, res, callback, requiredBodyValues));
+                this.serv.router.post(path, (req, res) => this.defaultHandler(req, res, callbackOpts.callback, callbackOpts.requiredBodyValues));
                 break;
             case RequestType.PATCH: 
-                this.serv.router.patch(path, (req, res) => this.defaultHandler(req, res, callback, requiredBodyValues));
+                this.serv.router.patch(path, (req, res) => this.defaultHandler(req, res, callbackOpts.callback, callbackOpts.requiredBodyValues));
                 break;
             case RequestType.OPTIONS: 
-                this.serv.router.options(path, (req, res) => this.defaultHandler(req, res, callback, requiredBodyValues));
+                this.serv.router.options(path, (req, res) => this.defaultHandler(req, res, callbackOpts.callback, callbackOpts.requiredBodyValues));
                 break;
             default: return false;
         }
@@ -73,13 +72,19 @@ class RouteRegistrar {
         return true;
     }
 
-    public async register(path: string, type: RequestType, defCallback: RouteCallback, { adminCallback, requiredBodyValuesDefault, requiredBodyValuesAdmin }: RouteRegisterOptions): Promise<boolean> {
+    public async register(path: string, type: RequestType, defCallbackOpts: RouteCallbackOptions, adminCallbackOpts?: RouteCallbackOptions): Promise<boolean> {
         // setup results (admin is true cuz it may not exec)
         let def_res = false;
         let admin_res = true;
         try {
-            def_res = this.registerRoute({ path, type, callback: defCallback, requiredBodyValues: requiredBodyValuesDefault });
-            if(adminCallback) admin_res = this.registerAdmin({ path, type, callback: adminCallback, requiredBodyValues: requiredBodyValuesAdmin });
+            def_res = this.registerRoute({ path, type, callbackOpts: {
+                callback: defCallbackOpts.callback,
+                requiredBodyValues: defCallbackOpts.requiredBodyValues
+            } });
+            if(adminCallbackOpts) admin_res = this.registerAdmin({ path, type, callbackOpts: {
+                callback: adminCallbackOpts.callback,
+                requiredBodyValues: adminCallbackOpts.requiredBodyValues
+            }});
         } catch(e) {
             return Promise.reject(e);
         }
@@ -87,19 +92,19 @@ class RouteRegistrar {
         return def_res && admin_res;
     }
 
-    public async registerDefault({ path, type, callback, requiredBodyValues }: RegistrationProps): Promise<boolean> {
+    public async registerDefault({ path, type, callbackOpts }: RegistrationProps): Promise<boolean> {
         // error if path starts with /admin
         if(path.startsWith('/admin')) {
             return Promise.reject(`path "${path}" cannot start with /admin`);
         }
 
         // register route
-        return this.registerRoute({ path, type, callback, requiredBodyValues });
+        return this.registerRoute({ path, type, callbackOpts });
     }
 
-    public registerAdmin({ path, type, callback, requiredBodyValues }: RegistrationProps) {
+    public registerAdmin({ path, type, callbackOpts }: RegistrationProps) {
         // just register path with /admin in the front
-        return this.registerRoute({ path: `/admin${path}`, type, callback, requiredBodyValues });
+        return this.registerRoute({ path: `/admin${path}`, type, callbackOpts });
     }
 
     public getServer(): express.Application {
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import RouteRegistrar from './routes/registrar.ts';
 import conf from './config.ts';
 import { RequestType } from './routes/registrar.ts';
+import { generateResponse } from './util.ts';
 
 // setup server and middleware
 const serv = express();
@@ -10,13 +11,15 @@ serv.use(express.json({ limit: '25kb' }));
 // setup routes
 const rr = new RouteRegistrar(serv);
 
-rr.register('/register', RequestType.POST, (req, res) => {
-    return res.status(200).send('hey!');
+rr.register('/register', RequestType.POST, {
+    callback: (req, res) => {
+        return res.status(200).send(generateResponse(true, 'hi'));
+    }
 }, {
-    adminCallback: (req, res) => {
-        return res.status(400).send(req.body.sid);
+    callback: (req, res) => {
+        return res.status(400).send(generateResponse(false, 'no'));
     },
-    requiredBodyValuesAdmin: ['sid']
+    requiredBodyValues: ['sid']
 });
 
 serv.listen(conf.port, () => {
